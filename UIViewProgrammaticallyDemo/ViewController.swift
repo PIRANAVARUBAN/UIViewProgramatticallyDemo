8c1972c0b3f5d1c35ff92beb0a2e9ceb777f80f3
4Axis_Admin <ruban@4axissolutions.com>
2018-11-18 07:34:59 +0530

    Initial Commit

---
+++ UIViewProgrammaticallyDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewProgrammaticallyDemo
+//
+//  Created by 4Axis_Admin on 11/18/18.
+//  Copyright © 2018 com.npruban.tutorials. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
