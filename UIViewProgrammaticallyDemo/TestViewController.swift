7746a83f85ebefccdf4aa838a43f99f6c129452f
Piranavaruban <mrtnpruban1992@gmail.com>
2018-11-30 11:08:14 +0530

    Presenting View Controller + Creating Constraints with Layout Anchors

--- UIViewProgrammaticallyDemo/TestViewController.swift
+++ UIViewProgrammaticallyDemo/TestViewController.swift
@@ -12,20 +12,54 @@ import UIKit
 
 class TestViewController: UIViewController, UINavigationControllerDelegate{
     
-    
-
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // self.navigationController?.navigationBar.isHidden = false
-        print("fgsfgfs")
+
         view.backgroundColor = UIColor.cyan
-        //configure
+        configureUI()
+    }
+    
+    
+    func configureUI(){
+        
+        //Creating Constraints with Layout Anchors
+        
+        self.view.addSubview(testPresentControllerButton)
+        
+        testPresentControllerButton.centerXAnchor.constraint(equalToSystemSpacingAfter: self.view.centerXAnchor, multiplier: 1.0).isActive = true
+        testPresentControllerButton.centerYAnchor.constraint(equalToSystemSpacingBelow: self.view.centerYAnchor, multiplier: 1.0).isActive = true
+        
+        testPresentControllerButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        testPresentControllerButton.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        
+    }
+    
+    
+    lazy var testPresentControllerButton: UIButton = {
+        
+        let button = UIButton()
+        button.backgroundColor = UIColor.magenta
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(testPresentControllerButtonTapped), for: .touchUpInside)
+        
+        return button
+        
+    }()
+    
+    
+    @objc func testPresentControllerButtonTapped() {
+        
+        let vc = TestPresentationViewController()
+        vc.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        self.present(vc, animated: true, completion: nil)
+        
     }
     
     
